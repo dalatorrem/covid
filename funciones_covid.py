36f129e32ad82905aba0756d7761cfa645c1eb44
dalatorrem <dalatorrem@unal.edu.co>
2020-04-07 12:29:41 -0500

    Update funciones_covid.py

--- funciones_covid.py
+++ funciones_covid.py
@@ -11,7 +11,7 @@ import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import display
+from IPython.display import display
 
 #####################################################################
 #### Función para visualizar
